8649b4a2942702c16d9877870948bdfacbb20ee4
中村泰輔 <nakamurayasusuke@MacBook-Pro.local>
2019-08-15 17:16:49 +0900

    調整

--- Card/TableViewCell.swift
+++ Card/TableViewCell.swift
@@ -10,15 +10,23 @@ import UIKit
 
 class TableViewCell: UITableViewCell {
 
+    // 顔の画像を受け取る変数
+    @IBOutlet weak var faceImageView: UIImageView!
+    // 名前ラベル
+    @IBOutlet weak var nameLabel: UILabel!
+    // 職業ラベル
+    @IBOutlet weak var occupationLabel: UILabel!
+    // 出身地ラベル
+    @IBOutlet weak var birthplaceLabel: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
     }
     
 }
